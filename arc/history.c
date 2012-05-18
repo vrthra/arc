839eb8f8680c198665f35fa94ba520d0126ab4da
vrtra <9@vrtra.net>
2012-05-18 12:27:32 -0700

    Has history now, it is stored in $% as an array such that the last command is always $%(1).

--- arc/history.c
+++ arc/history.c
@@ -14,22 +14,21 @@ treetostr(tree *t, char* buf){
 	rewind(o);
 	while(1) {
 		*buf = rchr(o);
-		if (*buf == 0) break;
-		buf++;
+		if (!*buf) break;
+		++buf;
 	}
 	closeio(o);
 	return start;
 }
 void
 rchistory(tree *t){
-	char buf[2048];
-	struct var *hist;
-	struct word *histval;
+	char buf[NBUF];
+	struct word *oldw = (word*)0;
 	if(!runq->iflag || !t)
 		return;
 	treetostr(t, buf);
-	fprint(2, "<%s>\n", vlook("%")->val->word);
-	/*setvar("%", newword(buf, vlook("%")->val));*/
-	setvar("%", newword(buf, (word*)0));
+	oldw = copywords(vlook("%")->val, oldw);
+	setvar("%", newword(vlook("%%")->val->word, oldw));
+	setvar("%%", newword(buf, (word*)0));
 }
 
